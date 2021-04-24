f02faa49cd1c31d0428eda98c5a3bb3894fa9fdc
Armand Sallé <armand.salle@gmail.com>
2021-04-24 12:49:25 +0200

    remove gatsby

--- src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react"
-import Test from "components/test"
-import "styles/main.scss"
-
-const IndexPage = (): JSX.Element => {
-  return <Test name="prout" />
-}
-
-export default IndexPage
