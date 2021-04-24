e7b135602a653ad21590d757a82e84973890c062
Armand Sallé <armand.salle@gmail.com>
2021-04-24 14:15:39 +0200

    styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app'
-import '../styles/globals.css'
+import '@/styles/main.scss'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,13 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home(): JSX.Element {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Hello</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <a href="http://google.com" target="_blank" rel="noreferrer">
-        hello
-      </a>
+      <h1>Hello</h1>
     </div>
   )
 }
