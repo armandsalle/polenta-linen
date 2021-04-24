f9de834a4ddff8b2892152828f4380d255020b46
Armand Sallé <armand.salle@gmail.com>
2021-04-24 13:01:03 +0200

    setup TS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <a href="http://google.com" target="_blank">
+      <a href="http://google.com" target="_blank" rel="noreferrer">
         hello
       </a>
     </div>
