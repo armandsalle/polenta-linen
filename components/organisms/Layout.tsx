afc0107d7c99254a0faba12a78725bc666a84f6f
Arthur Sallé <arthursalle.pro@gmail.com>
2021-04-25 11:12:50 +0200

    import fonts

--- components/organisms/Layout.tsx
+++ components/organisms/Layout.tsx
@@ -9,11 +9,16 @@ const Layout = ({ children, SEO }: LayoutProps): JSX.Element => {
   return (
     <>
       <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;1,400;1,500"
+          rel="stylesheet"
+        />
         <title>{SEO ? SEO : 'Polenta & Linen'}</title>
       </Head>
 
       <main>
-        <nav>Nav</nav>
+        <nav>Prout plop plip</nav>
         {children}
         <footer>Footer</footer>
       </main>
