e677d23a5e29bc66af0bfd9eb26e65a7e14f898e
Armand Sallé <armand.salle@gmail.com>
2021-04-25 21:38:45 +0200

    add SEO

--- components/organisms/Layout.tsx
+++ components/organisms/Layout.tsx
@@ -1,13 +1,17 @@
-import Head from 'next/head'
+import type { SEOContent } from '@/lib/queries/SEO/types'
+
 import Header from '@/components/organisms/Header'
 import Footer from '@/components/organisms/Footer'
+import Head from 'next/head'
 
 type LayoutProps = {
   children: React.ReactNode
-  SEO?: string
+  SEO: SEOContent
 }
 
 const Layout = ({ children, SEO }: LayoutProps): JSX.Element => {
+  const seoTitle = SEO ? SEO.title : 'Polenta & Linen'
+
   return (
     <>
       <Head>
@@ -16,7 +20,17 @@ const Layout = ({ children, SEO }: LayoutProps): JSX.Element => {
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;1,400;1,500"
           rel="stylesheet"
         />
-        <title>{SEO ? SEO : 'Polenta & Linen'}</title>
+
+        <title>{seoTitle}</title>
+        <meta name="description" content={SEO.description} />
+
+        <meta property="og:title" content={seoTitle} />
+        <meta property="og:description" content={SEO.description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content={SEO.ogImage.url} />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content={seoTitle} />
+        <meta name="twitter:description" content={SEO.description} />
       </Head>
 
       <main>
