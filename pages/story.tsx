a9e0553b52386882c439dfb0415a9298e5f1e878
Armand Sallé <armand.salle@gmail.com>
2021-05-02 13:54:46 +0200

    Swap destop to mobile first

--- pages/story.tsx
+++ pages/story.tsx
@@ -4,6 +4,7 @@ import type { Story as StoryType } from '@/lib/queries/story/types'
 import { client } from '@/lib/client'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import ResponsiveImage from '@/components/atoms/ResponsiveImage'
+import Title from '@/components/atoms/Title'
 
 type StoryProps = {
   story: StoryType
@@ -11,18 +12,21 @@ type StoryProps = {
 
 const Story = ({ story }: StoryProps): JSX.Element => {
   return (
-    <div>
-      <h1>{story.title}</h1>
-      <p>{story.seo.description}</p>
-      <p>{story.seo.title}</p>
-      <p>{story.seo.ogImage.url}</p>
-      {documentToReactComponents(story.description.json)}
+    <div className="container --story">
       <ResponsiveImage
         className="m"
         src={story.photo.url}
         alt={story.photo.title}
       />
-      <p>{story.signature}</p>
+      <section className="story-content">
+        <Title isSplit={true} as="h1">
+          {story.title}
+        </Title>
+        <div className="story-text">
+          {documentToReactComponents(story.description.json)}
+        </div>
+        <p className="signature">{story.signature}</p>
+      </section>
     </div>
   )
 }
