fa42d44d825ab6dc45993807bcefce3695d22aab
Armand Sallé <armand.salle@gmail.com>
2021-05-02 20:23:18 +0200

    Add Swiper Slider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from 'next/app'
 
 import Layout from '@/components/organisms/Layout'
 import '@/styles/globals.scss'
+import 'swiper/swiper.min.css'
+// import 'swiper/components/pagination/pagination.min.css'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
