122be98b082c07bb81901ed634dab5a3db6e1a74
Arthur Sallé <arthursalle.pro@gmail.com>
2021-05-03 22:07:56 +0200

    create navbar effect on scroll

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -1,10 +1,24 @@
 import Burger from '@/components/atoms/nav/Burger'
 import SearchIcon from '@/components/atoms/search/SearchIcon'
 import PolentaIcon from '@/components/atoms/polenta/PolentaIcon'
+import { useEffect, useState } from 'react'
 
 const Header = (): JSX.Element => {
+  const [navbar, setNavbar] = useState(false)
+
+  const changeBackground = () => {
+    window.scrollY >= 180 ? setNavbar(true) : setNavbar(false)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', changeBackground)
+    return () => {
+      window.removeEventListener('scroll', () => changeBackground)
+    }
+  }, [changeBackground])
+
   return (
-    <header className="header">
+    <header className={navbar ? 'header header__scroll' : 'header'}>
       <Burger />
       <PolentaIcon />
       <SearchIcon />
