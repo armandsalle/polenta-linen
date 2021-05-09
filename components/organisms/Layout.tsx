d11fa1a61d5d4d0aae87b6dd5245fdf55627a960
Arthur Sallé <arthursalle.pro@gmail.com>
2021-05-09 18:50:50 +0200

    fix accessibility lighthouse

--- components/organisms/Layout.tsx
+++ components/organisms/Layout.tsx
@@ -22,7 +22,7 @@ const Layout = ({
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;1,400;1,500"
+            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;1,400;1,500&display=swap"
             rel="stylesheet"
           />
 
